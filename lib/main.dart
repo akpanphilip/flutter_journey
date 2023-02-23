b36e1c8a0be7ff4c0ed5515e12837fb343368570
akpanphilip <akpanphilip1122@gmail.com>
2023-02-23 23:55:16 +0100

    working with images

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'screens/images.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
